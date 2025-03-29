bd33e385b0c14441c88374bec7d2e3b98b1fc80b
Enoch18 <32671904+Enoch18@users.noreply.github.com>
2025-03-29 12:24:28 +0200

    changes

--- components/Transactions.tsx
+++ components/Transactions.tsx
@@ -17,7 +17,7 @@ const Transactions = ({transactions}: TransactionProps) => {
     }
 
     return (
-        <div className="mt-10 max-h-[65vh] overflow-scroll">
+        <div className="mt-10">
             <div className="flex flex-row items-center">
                 <h4 className="text-lg font-semibold">Transactions</h4>
                 <div className="flex flex-row flex-1 justify-end">
@@ -25,21 +25,23 @@ const Transactions = ({transactions}: TransactionProps) => {
                 </div>
             </div>
 
-            {transactions.length === 0 && <p>No transactions on this account!</p>}
+            <div className="max-h-[60vh] overflow-scroll">
+                {transactions.length === 0 && <p>No transactions on this account!</p>}
 
-            {transactions?.length > 0 && transactions?.map((item:any, index:number) => (
-                <div className="flex flex-row items-center border border-gray-200 p-3 rounded mt-4" key={index}>
-                    <div>
-                        <p className="font-semibold capitalize">Fund {item.type}</p>
-                        <p className={`font-semibold text-${transactionColor(item.type)}`}>${item.amount}</p>
+                {transactions?.length > 0 && transactions?.map((item:any, index:number) => (
+                    <div className="flex flex-row items-center border border-gray-200 p-3 rounded mt-4" key={index}>
+                        <div>
+                            <p className="font-semibold capitalize">Fund {item.type}</p>
+                            <p className={`font-semibold text-${transactionColor(item.type)}`}>${item.amount}</p>
+                            
+                        </div>
                         
+                        <div className="flex flex-row items-center justify-end flex-1">
+                            <p className="text-sm mt-1 italic">{item.created_at}</p>
+                        </div>
                     </div>
-                    
-                    <div className="flex flex-row items-center justify-end flex-1">
-                        <p className="text-sm mt-1 italic">{item.created_at}</p>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
